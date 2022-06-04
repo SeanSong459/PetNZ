b05f92357b11bb8123f61ea5893b38c9ac4b8ae5
Dexuan Song <dson459@outlook.com>
2022-06-04 15:09:50 +1200

    added Hero section

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroImg from "../../Assets/images/HeroImg.png";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="HeroContainer">
+      {/* <img src={HeroImg} alt="Heroimg" className="HeroImg"></img> */}
+    </div>
+  );
+};
+
+export default Hero;
